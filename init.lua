e8a61c87e1a27c4c956276269dbd3b72bc076ac3
fakefacedance <chernenok.pv@gmail.com>
2025-01-28 10:10:26 +0300

    fix formatters config

--- init.lua
+++ init.lua
@@ -742,13 +742,13 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        php = { { 'php_cs_fixer', 'pint' } },
+        php = { 'pint', 'php_cs_fixer', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettier', 'prettierd' } },
+        javascript = { 'prettier', 'prettierd', stop_after_first = true },
         html = { 'prettier' },
       },
     },
